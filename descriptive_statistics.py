632cd8dcae59d1eefa47eae4f306ebf782fc6bc8
luyanghua <43014945+luyanghua@users.noreply.github.com>
2018-09-30 18:19:40 -0400

    Update descriptive_statistics.py

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -5,3 +5,15 @@ iris_dataset = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/ir
 
 iris = np.recfromcsv(iris_dataset, encoding=None)  # in numpy
 iris = pd.read_csv(iris_dataset)  # in pandas
+
+iris_setosa = iris['species'] == "setosa"
+iris_versicolor = iris['species'] == "versicolor"
+iris_virginica = iris['species'] == "virginica"
+
+print(iris[iris_setosa].mean())
+print(iris[iris_versicolor].mean())
+print(iris[iris_virginica].mean())
+
+print(iris[iris_setosa].std())
+print(iris[iris_versicolor].std())
+print(iris[iris_virginica].std())
